d8172ae9c3f8a0849cb7bfd9c492c9ed85045de5
ByungjunKang <rivert88@gmail.com>
2023-01-09 14:59:19 +0900

    modify shortcut

--- theme/community/head.php
+++ theme/community/head.php
@@ -150,9 +150,9 @@ include_once(G5_THEME_LIB_PATH.'/new_lastest.lib.php');
 	    <div id="aside_ct">
 	        <div class="short_cut">
 	            <ul>
-	                <li class="f_short_cut"><strong><a href="#">바로가기 메뉴 1</a></strong></li>
-	                <li><strong><a href="#">바로가기 메뉴 2</a></strong></li>
-	                <li><strong><a href="#">바로가기 메뉴 3</a></strong></li>
+	                <li class="f_short_cut"><strong><a href="https://naver.com">네이버</a></strong></li>
+	                <li><strong><a href="http://3.34.99.214/bbs/board.php?bo_table=notice">공지게시판</a></strong></li>
+	                <li><strong><a href="http://3.34.99.214/bbs/board.php?bo_table=free">자유게시판</a></strong></li>
 	            </ul>
 	        </div>
 	        <div class="aside_lt">
